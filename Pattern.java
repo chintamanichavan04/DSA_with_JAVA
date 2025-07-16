19fdc216fdec2cb612486cc86f909c9c846b68ca
chintamanichavan04 <grcgopal7183@gmail.com>
2025-07-16 12:08:49 +0530

    java

--- Pattern.java
+++ Pattern.java
@@ -18,6 +18,7 @@ public class Pattern {
     // * * * 
     // * * * * 
     // * * * * * 
+    
     static void pattern2(int n){
         for(int i=0; i<n; i++){
             for(int j=0; j<i+1; j++){
